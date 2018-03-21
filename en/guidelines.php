15dd8bbaa56e31bafb4b0e8a85d44a71d4b36e5d
Albert Leitato <wizqydy@gmail.com>
2018-03-21 13:15:20 +0300

    Add markdown parser for textarea inputs and do a little clean uup

---
+++ en/guidelines.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'students' => [
+        'header' => 'Download  the sample file and add student data and then upload the updated file',
+        'title' => 'Guidelines For filling the file',
+        'li1' => 'Do not modify the column headers',
+        'li2' => 'Date format should be dd-mm-yyyy eg '.date('d-m-Y'),
+        'li3' => 'Use the section tag for a class e.g for Class one West use class-one-west',
+    ],
+];
